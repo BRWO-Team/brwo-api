1479fccd996ee4416a98846560eca4d3b52910f4
unknown <taylor.alan.johnson@gmail.com>
2019-12-25 13:47:48 -0500

    add count, n items, user items, and site map routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, url_for
 import firebase_admin
 from firebase_admin import credentials, firestore
 import google.cloud
@@ -22,11 +22,50 @@ def index():
     return "You're home"
 
 
+@app.route('/api/v1.0/count/items', methods=['GET'])
+def get_count_items():
+    return str(len(list(store.collection('items').get())))
+
+
 @app.route('/api/v1.0/all/items', methods=['GET'])
 def get_all_items():
     items = []
 
     doc_ref = store.collection(u'items')  # .limit(2)
+
+    try:
+        docs = doc_ref.get()
+        for doc in docs:
+            items.append(doc.to_dict())
+    except google.cloud.exceptions.NotFound:
+        print(u'Missing data')
+
+    return jsonify({'items': items})
+
+
+@app.route('/api/v1.0/n/items', methods=['GET'])
+def get_n_items():
+    items = []
+    n = int(request.args.get('n'))
+
+    doc_ref = store.collection(u'items').limit(n)
+
+    try:
+        docs = doc_ref.get()
+        for doc in docs:
+            items.append(doc.to_dict())
+    except google.cloud.exceptions.NotFound:
+        print(u'Missing data')
+
+    return jsonify({'items': items})
+
+
+@app.route('/api/v1.0/user/items', methods=['GET'])
+def get_users_items():
+    items = []
+    uid = int(request.args.get('userid'))
+
+    doc_ref = store.collection(u'items').where(u'userid', u'==', uid)
 
     try:
         docs = doc_ref.get()
@@ -53,9 +92,27 @@ def geocode():
     return zip_code
 
 
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    return dict(links)
+
+
 if __name__ == '__main__':
     # test
-    #app.run(debug=True)
+    app.run(debug=True)
 
     # production
-    app.run(debug=False, port=8080)
+    # app.run(debug=False, port=8080)
