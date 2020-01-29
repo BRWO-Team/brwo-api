528cba8e794a42fa25b137fea2819f06f6c1303f
unknown <taylor.alan.johnson@gmail.com>
2020-01-28 21:48:21 -0500

    get swagger set up

--- app.py
+++ app.py
@@ -11,12 +11,26 @@ import json
 from geopy import distance
 import datetime
 from fuzzywuzzy import fuzz
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 # activate venv - brwo-venv\Scripts\activate
 
 app = Flask(__name__)
 CORS(app)
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "BRWO_API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 # FIREBASE
 # Use the application default credentials
